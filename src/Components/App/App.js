d60ad85a56deaaf90165b9098c2f93551ce953d0
ThomasPortolano <thomas.portolano@gmail.com>
2020-06-06 19:58:59 +0800

    Fixing App component render method

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -4,20 +4,23 @@ import './App.css';
 import { SearchBar } from './Searchbar/SearchBar';
 import { SearchResults } from './SearchResults/SearchResults';
 import { Playlist } from './Playlist/Playlist';
+import { render } from '@testing-library/react';
 
-function App() {
-  return (
+class App extends React.Component() {
+  return ()
+  
+  render(){
     <div>
   <h1>Ja<span className="highlight">mmm</span>ing</h1>
   <div className="App">
-    <!-- Add a SearchBar component -->
+    /* Add a SearchBar component */
     <div className="App-playlist">
-      <!-- Add a SearchResults component -->
-      <!-- Add a Playlist component -->
+    /* Add a SearchResults component */
+    /* Add a Playlist component */
     </div>
   </div>
 </div>
-  );
-}
+  }
+};
 
 export default App;
